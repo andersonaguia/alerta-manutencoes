da93dbaa211f279a67e2c2fbc0243fb9cf210c0e
Anderson Aguiar <andersonlaguiar@gmail.com>
2023-02-08 14:54:10 -0300

    refactor: format all preventives response

--- src/preventive/services/findAll-preventive.service.ts
+++ src/preventive/services/findAll-preventive.service.ts
@@ -15,12 +15,28 @@ export class FindAllPreventiveService {
       try {
         const allPreventives = await this.preventiveRepository.find();
         if (allPreventives.length > 0) {
-          resolve(allPreventives);
+          resolve(this.formatResponse(allPreventives));
         }
         reject("Nenhuma manutenção preventiva encontrada");
       } catch (error) {
         reject(error);
       }
     })
+  }
+
+  formatResponse(preventives: PreventiveEntity[]): PreventiveEntity[] {
+    const response = preventives.map((preventive) => {
+      delete preventive.created_at;
+      delete preventive.updated_at;
+      delete preventive.frequency.created_at;
+      delete preventive.frequency.updated_at;
+      delete preventive.category.created_at;
+      delete preventive.category.updated_at;
+      delete preventive.responsible.created_at;
+      delete preventive.responsible.updated_at;
+      return preventive;
+    })
+
+    return response;
   }
 }
